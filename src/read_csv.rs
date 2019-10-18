7b0f550b424286bde9cac08c2c4435cd05357c53
jkmrto <juanka1331@gmail.com>
2019-10-18 15:32:41 +0200

    read file in function

--- src/read_csv.rs
+++ src/read_csv.rs
@@ -1,25 +1,24 @@
-use std::env;
+use std::fmt;
 use std::fs;
 
 fn main() {
     let filename = "./matrix.csv";
-    println!("In file {}", filename);
+    let matrix = read_matrix_from_file(&filename);
+    println!("{}", matrix[0][4])
+}
+ 
+fn read_matrix_from_file(filename: &str) -> Vec<Vec<u32>> {
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
 
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
-
-    let split = contents.split("\n");
-    let temp_vec: Vec<&str> = split.collect();
-
-    println!("Printing:");
-    for row in &temp_vec{
-        let split_row: Vec<u32>  = row // string
-            .split(" ") // Iter::vec<&str>
-            .map(|s| s.parse::<u32>().unwrap()) // Iter::vec<u32> 
+    let rows: Vec<&str>  = contents.split("\n").collect();
+    let mut splitted_rows: Vec<Vec<u32>> = Vec::new();
+    for row in rows {
+        println!("{}", row);
+        let splitted_row = row // string
+            .split(", ") // Iter::vec<&str>
+            .map(|s| s.parse::<u32>().unwrap()) // Iter::vec<u32>
             .collect(); // vec <u32>
-            
-         for eo in &split_row{
-             println!("{}", eo);
-         }
+        splitted_rows.push(splitted_row)
     }
+    splitted_rows
 }
