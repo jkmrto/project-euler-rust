0a27e4e1efed3c43351249dcc6215f4a67bbe2d8
jkmrto <juanka1331@gmail.com>
2019-10-18 23:01:18 +0200

    using read_csv from exercise_11

--- src/read_csv.rs
+++ src/read_csv.rs
@@ -1,19 +1,12 @@
 use std::fmt;
 use std::fs;
 
-fn main() {
-    let filename = "./matrix.csv";
-    let matrix = read_matrix_from_file(&filename);
-    println!("{}", matrix[0][4])
-}
- 
-fn read_matrix_from_file(filename: &str) -> Vec<Vec<u32>> {
+pub fn read_matrix_from_file(filename: &str) -> Vec<Vec<u32>> {
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
 
-    let rows: Vec<&str>  = contents.split("\n").collect();
+    let rows: Vec<&str> = contents.split("\n").collect();
     let mut splitted_rows: Vec<Vec<u32>> = Vec::new();
     for row in rows {
-        println!("{}", row);
         let splitted_row = row // string
             .split(", ") // Iter::vec<&str>
             .map(|s| s.parse::<u32>().unwrap()) // Iter::vec<u32>
