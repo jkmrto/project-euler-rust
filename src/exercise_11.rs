03b0765cbcc2ad2791e678abc0d469d90ea9dc4b
jkmrto <juanka1331@gmail.com>
2019-10-19 16:24:59 +0200

    added find_max_product_by_swap with test

--- src/exercise_11.rs
+++ src/exercise_11.rs
@@ -6,9 +6,10 @@ pub struct Position {
 }
 
 impl Position {
-    fn new_with_shift(shift: &Position, moves: usize) -> Position {
+    fn new(origin: &Position, shift: &Position, moves: usize) -> Position {
         let mut position = Position { x: 0, y: 0 };
         position.several_shift(shift, moves);
+        position.shift(origin);
         position
     }
 
@@ -40,8 +41,8 @@ impl PartialEq for Position {
     }
 }
 
-fn are_valid_index(positions: &[Position; 3], limit: &Position) -> bool {
-    (limit.x > positions[2].x) && (limit.y > positions[2].y)
+fn is_valid_index(position: &Position, matrix: &Vec<Vec<u32>>) -> bool {
+    (matrix[0].len() > position.x) && (matrix.len() > position.y)
 }
 
 fn update_positions(positions: &mut [Position; 3], shift: &Position) {
@@ -62,27 +63,20 @@ fn calculate_product(matrix: &Vec<Vec<u32>>, positions: &[Position; 3]) -> u32 {
     acc
 }
 
-fn init_positions(shift: &Position) -> [Position; 3] {
+fn init_positions(origin: &Position, shift: &Position) -> [Position; 3] {
     [
-        Position::new_with_shift(shift, 0),
-        Position::new_with_shift(shift, 1),
-        Position::new_with_shift(shift, 2),
+        Position::new(origin, shift, 0),
+        Position::new(origin, shift, 1),
+        Position::new(origin, shift, 2),
     ]
 }
 
-fn find_max_product(matrix: &Vec<Vec<u32>>, shift: &Position) -> u32 {
-    // build positions based on shift
-    let mut positions = init_positions(shift);
-
-    let limit = Position {
-        y: matrix.len(),
-        x: matrix[0].len(),
-    };
-
+fn find_max_product(matrix: &Vec<Vec<u32>>, origin: &Position, shift: &Position) -> u32 {
+    let mut positions = init_positions(origin, shift);
     let mut max_product = 0;
     let mut product;
 
-    while are_valid_index(&positions, &limit) {
+    while is_valid_index(&positions[2], &matrix) {
         product = calculate_product(&matrix, &positions);
         max_product = if max_product < product {
             product
@@ -100,22 +94,35 @@ mod tests {
 
     #[test]
     fn init_positions() {
+        let origin = Position { x: 0, y: 0 };
         let shift = Position { x: 1, y: 1 };
-        let positions = super::init_positions(&shift);
+        let positions = super::init_positions(&origin, &shift);
         assert_eq!(positions[0], Position { x: 0, y: 0 });
         assert_eq!(positions[1], Position { x: 1, y: 1 });
         assert_eq!(positions[2], Position { x: 2, y: 2 });
     }
 
     #[test]
+    fn init_positions_with_shifted_origin() {
+        let origin = Position { x: 1, y: 1 };
+        let shift = Position { x: 1, y: 0 };
+        let positions = super::init_positions(&origin, &shift);
+        assert_eq!(positions[0], Position { x: 1, y: 1 });
+        assert_eq!(positions[1], Position { x: 2, y: 1 });
+        assert_eq!(positions[2], Position { x: 3, y: 1 });
+    }
+
+    #[test]
     fn horizontal() {
+        let origin = Position { x: 0, y: 0 };
         let shift = Position { x: 1, y: 0 };
         let matrix = vec![vec![1, 2, 3, 4, 5, 5, 5, 1, 1, 1]];
-        assert_eq!(super::find_max_product(&matrix, &shift), 125);
+        assert_eq!(super::find_max_product(&matrix, &origin, &shift), 125);
     }
 
     #[test]
     fn main_diagonal() {
+        let origin = Position { x: 0, y: 0 };
         let matrix = vec![
             vec![5, 2, 3, 1, 1],
             vec![1, 2, 3, 1, 1],
@@ -124,7 +131,36 @@ mod tests {
             vec![1, 2, 3, 1, 1],
         ];
         let shift = Position { x: 1, y: 1 };
-        assert_eq!(super::find_max_product(&matrix, &shift), 30);
+        assert_eq!(super::find_max_product(&matrix, &origin, &shift), 30);
     }
 
+    #[test]
+    fn all_horizontal() {
+        let mut origin = Position { x: 0, y: 0 };
+        let swap_shift = Position { x: 0, y: 1 };
+        let shift = Position { x: 1, y: 0 };
+
+        let matrix = vec![
+            vec![5, 2, 3, 1, 1],
+            vec![1, 2, 3, 1, 1],
+            vec![1, 2, 3, 1, 1],
+            vec![10, 2, 3, 3, 1],
+            vec![1, 2, 3, 1, 1],
+        ];
+
+        let mut product = 0;
+        let mut max_product = 0;
+
+        while super::is_valid_index(&origin, &matrix) {
+            product = super::find_max_product(&matrix, &origin, &shift);
+            max_product = if max_product < product {
+                product
+            } else {
+                max_product
+            };
+
+            origin.shift(&swap_shift);
+        }
+        assert_eq!(max_product, 60);
+    }
 }
